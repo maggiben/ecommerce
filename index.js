34989f953abaf148b025bab014e781e7c611008c
maggiben <benjaminmaggi@gmail.com>
2016-06-21 18:13:06 -0300

    auto es6

--- index.js
+++ index.js
@@ -1,24 +1,2 @@
-import express from 'express';
-import graphqlHTTP from 'express-graphql';
-import schema from './Models/schema';
-import Loader from './Models/loaders';
-
-const app = express();
-
-app.use(graphqlHTTP(req => {
-  const loaders = {
-    product: Loader().product,
-    category: Loader().category
-  };
-
-  return {
-    context: {loaders},
-    graphiql: true,
-    schema,
-  };
-}));
-
-app.listen(
-  5000,
-  () => console.log('GraphQL Server running at http://localhost:5000')
-);
+require('babel-register');
+require('./server.js');
